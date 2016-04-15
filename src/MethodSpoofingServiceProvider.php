a4e37753fdd54e1bdf1d96b404bd1cee81d664ad
Anael Favre <a.favre@web-6.fr>
2016-04-15 15:57:12 +0200

    Removed comment

--- src/MethodSpoofingServiceProvider.php
+++ src/MethodSpoofingServiceProvider.php
@@ -8,14 +8,12 @@ class MethodSpoofingServiceProvider
 {
 	public function register()
 	{
-		/*
 		if(isset($_GET['_method'])) {
 			$_SERVER['REQUEST_METHOD'] = strtoupper($_GET['_method']);
 		}
 		if(isset($_POST['_method'])) {
 			$_SERVER['REQUEST_METHOD'] = strtoupper($_POST['_method']);
 		}
-		*/
 	}
 
 	public function boot()
